25b145bf18efc2088baf3caff0ae8aa3ba667c68
hhefesto <daniel.herrera.rendon@gmail.com>
2016-10-20 18:05:08 -0500

    ejemplo Make It Rational resuelto en matlab

--- ejemplo_matlab/eje.m
+++ ejemplo_matlab/eje.m
@@ -1,7 +1,36 @@
-CxC = [1 2 1/3 1/3 1/4; 1/2 1 1/4 1/3 1/5; 3 3 1/2 1 1/3; 4 5 2 2 1];
+CxC = [1 2 1/3 1/3 1/4; 1/2 1 1/4 1/3 1/5; 3 4 1 2 1/2 ;3 3 1/2 1 1/2; 4 5 2 2 1];
 
 Cost = [1 17/4 17/9; 4/17 1 4/9; 9/17 9/4 1];
 Ease = [1 1/2 3; 2 1 3; 1/3 1/3 1];
 FinB = [1 86/67 86/35; 67/86 1 67/35; 35/86 35/67 1];
 ResIm = [1 1 2; 1 1 2; 1/2 1/2 1];
 StraVal = [1 1/3 2; 3 1 5; 1/2 1/5 1];
+
+
+[V,D] = eig(CxC)
+EigV_CxC = V(:,1)
+EigV_CxC_W = EigV_CxC / sum(EigV_CxC)
+
+[V,D] = eig(Cost)
+EigV_Cost = V(:,1)
+EigV_Cost_W = EigV_Cost / sum(EigV_Cost)
+
+[V,D] = eig(Ease)
+EigV_Ease = V(:,1)
+EigV_Ease_W = EigV_Ease / sum(EigV_Ease)
+
+[V,D] = eig(FinB)
+EigV_FinB = V(:,2)
+EigV_FinB_W = EigV_FinB / sum(EigV_FinB)
+
+[V,D] = eig(ResIm)
+EigV_ResIm = V(:,2)
+EigV_ResIm_W = EigV_ResIm / sum(EigV_ResIm)
+
+[V,D] = eig(StraVal)
+EigV_StraVal = V(:,1)
+EigV_StraVal_W = EigV_StraVal / sum(EigV_StraVal)
+
+M = [EigV_Cost_W EigV_Ease_W EigV_FinB_W EigV_ResIm_W EigV_StraVal_W]
+
+Res = M * EigV_CxC_W
